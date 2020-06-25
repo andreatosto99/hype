776e1f9c8e9c6aad9c31d09b91d98211bbbc9243
Andrea Tosto <andrea.tosto.99@gmail.com>
2020-06-25 19:22:15 +0200

    aggiunta di immagine logo (provvisoria)

--- hypeBrand/src/app/app-routing.module.ts
+++ hypeBrand/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'registrazione',
+    loadChildren: () => import('./registrazione/registrazione.module').then( m => m.RegistrazionePageModule)
+  },
 ];
 
 @NgModule({
